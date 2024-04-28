3c26a62f5744a4863a86b8c693708e79e7de87a9
Sreeni-31 <sreenifarm@gmail.com>
2024-04-29 00:11:51 +1200

    updated the API version

--- app/src/main/java/com/altruists/nzbirdsguide/MainActivity.java
+++ app/src/main/java/com/altruists/nzbirdsguide/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+	//Calling first layout of the app
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
